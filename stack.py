3331ad1c858e8d66c51e2d365e7cb3655b5edd31
Anna <strzodanna@gmail.com>
2021-09-07 20:56:33 +0200

    make depth and top attributes protected. Stack implementation completed.

--- stack.py
+++ stack.py
@@ -1,7 +1,5 @@
-
-
 class Node:
-    def __init__(self, value, parent: "Node" = None):
+    def __init__(self, value, parent):
         self.value = value
         self.parent = parent
 
@@ -9,34 +7,42 @@ class Node:
 class Stack:
     def __init__(self, *items):
         self._depth = 0
-        self._last_node = None
+        self.__last_node = None
         for item in items:
             self.push(item)
 
     def push(self, new_element):
-        self._last_node = Node(new_element, parent=self._last_node)
+        self.__last_node = Node(new_element, parent=self.__last_node)
         self._depth += 1
 
     @property
+    def depth(self):
+        return self._depth
+
+    @property
     def top(self):
-        if self._last_node is None:
+        if self.__last_node is None:
             raise ValueError("Empty stack has no top")
         else:
-            return self._last_node.value
+            return self.__last_node.value
 
     def pop(self):
-        if self._last_node is None:
+        if self.__last_node is None:
             raise ValueError("Empty stack cannot be popped")
         else:
-            popped_element = self._last_node.value
-            self._last_node = self._last_node.parent
+            popped_element = self.__last_node.value
+            self.__last_node = self.__last_node.parent
             self._depth -= 1
             return popped_element
 
 
 if __name__ == '__main__':
     stack = Stack(1)
-    stack.push(2)
+    # stack.push(2)
     stack.pop()
-    stack.pop()
+    # stack.pop()
+    # print(stack.top)
+    # stack.depth = 0 - nie da się
+    # stack.top = 5 - nie da się
     print(stack.top)
+    print(stack.depth)
--- test_stack.py
+++ test_stack.py
@@ -1,7 +1,5 @@
 import pytest
-
 from stack import Stack
-# Test cases for an empty stack
 
 
 def test_an_empty_stack_has_depth_equals_to_zero():
@@ -33,20 +31,18 @@ def test_pushing_an_element_on_stack_sets_this_element_as_top():
     assert stack.top == "a"
 
 
-
-# def test_popping_elements_from_stack_created_from_an_iterable_returns_them_in_reversed_order():
-#     stack = Stack((1, 2, 3))
-#
-#     assert [stack.pop() for _ in range(3)] == [3, 2, 1]
-#
-#
+def test_popping_an_element_results_in_put_at_the_top_the_previous_one():
+    stack = Stack(1, 2, 3)
+    stack.pop()
+    stack.pop()
+    assert stack.top == 1
 
 
 @pytest.mark.parametrize(
     "initial_stack, expected_element",
     [
         (Stack(1, 2, 3), 3),
-        (Stack(0), 0)
+        (Stack("c"), "c")
     ]
 )
 def test_popping_element_from_nonempty_stack_decreases_depth_and_returns_last_element(
@@ -55,3 +51,5 @@ def test_popping_element_from_nonempty_stack_decreases_depth_and_returns_last_el
     initial_depth = initial_stack._depth
     assert initial_stack.pop() == expected_element
     assert initial_stack._depth == initial_depth - 1
+
+
